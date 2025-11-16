bdd3991455e345440293638744cf582a3aa9d8d8
Abragus <gustav.a.abrahamsson@gmail.com>
2025-11-16 17:48:43 +0100

    Remove apply-cancel by config disable in super method

--- modules/__syncsmith_module.py
+++ modules/__syncsmith_module.py
@@ -2,11 +2,8 @@ class SyncsmithModule:
     def __init__(self, name):
         self.name = name
 
-    def apply(self, config=None, dry_run=False):
-        if not config.get("enabled", False):
-            return
-        
-        print(f"Applying module: {self.name} with config: {config} {("dry_run=true" if dry_run else "")}")
+    def apply(self, config=None, dry_run=False):        
+        print(f"Applying module: {self.name} {("dry_run=true" if dry_run else "")}")
 
-    def generate_config_stub(self):
+    def generate_config_stub(self, local_cfg):
         return {}
