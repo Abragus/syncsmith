1cd4b88681d4474e43822b630082a74ec066036d
Abragus <gustav.a.abrahamsson@gmail.com>
2025-11-16 17:47:41 +0100

    Detect if syncsmith is installed in current dir instead of default

--- syncsmith.sh
+++ syncsmith.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 PY=python3
-INSTALL_DIR="${INSTALL_DIR:-/opt/syncsmith}"
+if [[ -f "$(dirname "$0")/syncsmith.py" ]]; then
+    INSTALL_DIR="$(dirname "$0")"
+else
+    INSTALL_DIR="${INSTALL_DIR:-/opt/syncsmith}"
+fi
+
 cd "$INSTALL_DIR" || { echo "[syncsmith] Failed to enter install directory"; exit 1; }
 $PY syncsmith.py "$@"
