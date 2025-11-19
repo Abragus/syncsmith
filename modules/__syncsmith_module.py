2db9d53e5a167a8390a3c6d4b4ae8f2828b64887
Gustav Abrahamsson <gustav.a.abrahamsson@gmail.com>
2025-11-19 18:07:44 +0100

    Rename local_cfg -> env

--- modules/__syncsmith_module.py
+++ modules/__syncsmith_module.py
@@ -5,5 +5,5 @@ class SyncsmithModule:
     def apply(self, config=None, dry_run=False):        
         print(f"Applying module: {self.name} {("dry_run=true" if dry_run else "")}")
 
-    def generate_config_stub(self, local_cfg):
+    def generate_config_stub(self, env):
         return {}
