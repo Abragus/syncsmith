75ae026dc1cd5df8712a2ff273cfdc3fb9e57f0f
Gustav Abrahamsson <gustav.a.abrahamsson@gmail.com>
2025-12-29 04:03:59 +0100

    Add module to download file using curl

---
+++ modules/curl.py
@@ -0,0 +1,25 @@
+import os
+from modules.__syncsmith_module import SyncsmithModule
+from globals import COMPILED_FILES_DIR
+
+metadata = {
+    "name": "curl",
+    "description": "Clone a git repository to a target location",
+    "single_instance": False,
+}
+
+class Curl(SyncsmithModule):
+    def __init__(self, modulename=None):
+        super().__init__(modulename)
+
+    def apply(self, config, dry_run=False):
+        super().apply(config, dry_run=dry_run)
+
+        if dry_run:
+            print(f"[DRY RUN] Would curl {config['url']} to {config.get('path', '')}")
+            return
+        outfile = os.path.expanduser(config.get('path', ''))
+        curl_command = f"curl -o {COMPILED_FILES_DIR if not os.path.isabs(outfile) else ''}{outfile} {config['url']}"
+
+        print(f"Executing: {curl_command}")
+        os.system(curl_command)
