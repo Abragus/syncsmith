3129b120f1637af607094d0070530e30364edbc4
Gustav Abrahamsson <gustav.a.abrahamsson@gmail.com>
2025-12-29 03:30:27 +0100

    Add module to clone Git repos

---
+++ modules/git_clone.py
@@ -0,0 +1,35 @@
+import os, shutil
+from modules.__syncsmith_module import SyncsmithModule
+from utils.paths import get_syncsmith_root
+from globals import COMPILED_FILES_DIR
+
+metadata = {
+    "name": "git_clone",
+    "description": "Clone a git repository to a target location",
+    "single_instance": False,
+}
+
+class GitClone(SyncsmithModule):
+    def __init__(self, modulename=None):
+        super().__init__(modulename)
+
+    def apply(self, config, dry_run=False):
+        super().apply(config, dry_run=dry_run)
+
+        if dry_run:
+            print(f"[DRY RUN] Would clone into {config['url']}")
+            return
+        
+        clone_command = f"git clone {config['url']}"
+
+        if "branch" in config:
+            clone_command += f" -b {config['branch']}"
+
+        if not config.get("absolute_path", False):
+            clone_command += COMPILED_FILES_DIR
+        
+        if "path" in config:
+            clone_command += f" {config['path']}"
+            
+        print(f"Executing: {clone_command}")
+        os.system(clone_command)
